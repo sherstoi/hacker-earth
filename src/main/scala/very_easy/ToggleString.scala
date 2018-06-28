eda0c93e6144565a79a4f97c5c0d8fea9935f1cc
sherstoi <sherstoi83@gmail.com>
2018-06-29 01:07:03 +0200

    ToggleString

--- src/main/scala/very_easy/ToggleString.scala
+++ src/main/scala/very_easy/ToggleString.scala
@@ -3,11 +3,15 @@ package very_easy
 object ToggleString {
   def toggle(str: String): String = {
     str.foldLeft("")((accumulator, curSymbol) =>
-      if (curSymbol.isLower) { accumulator + curSymbol.toUpper.toString }
-      else  { accumulator + curSymbol.toLower.toString })
+      if (curSymbol.isLower) accumulator + curSymbol.toUpper else accumulator + curSymbol.toLower)
+  }
+
+  def toggleOptimized(str: String): String = { str.foldLeft(new StringBuilder)((stringBuilder, curValue) =>
+    if (curValue.isLower) stringBuilder append curValue.toUpper
+    else stringBuilder append curValue.toLower).mkString
   }
 
   def main(args: Array[String]): Unit = {
-    println(toggle("AbCdF"))
+    println(toggleOptimized("AbCdF"))
   }
 }
