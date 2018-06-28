83ad113820052ba8bcb712dff8c5371e5279924a
sherstoi <sherstoi83@gmail.com>
2018-06-29 01:50:04 +0200

    RoyAndProfilePicture

---
+++ src/main/scala/very_easy/RoyAndProfilePicture.scala
@@ -0,0 +1,13 @@
+package very_easy
+
+object RoyAndProfilePicture {
+  def calcDimension(l: Int, w: Int, h: Int): String = {
+    if (w > l && h > l) { if (w == h) { "ACCEPTED" } else { "CROP IT" } }
+    else if (w < l || h < l) { "UPLOAD ANOTHER" }
+    else { "UNKNOWN" }
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(calcDimension(12, 13, 12))
+  }
+}
