94c457f698f8fc33928764c77e41849f0114a6a0
sherstoi <sherstoi83@gmail.com>
2018-06-29 10:18:34 +0200

    TwoStrings

---
+++ src/main/scala/easy/TwoStrings.scala
@@ -0,0 +1,20 @@
+package easy
+
+object TwoStrings {
+  def isStringsIdentical(str1: String, str2: String): Boolean = {
+    if (str1.length == str2.length) isMapsEqual(statisticMap(str1), statisticMap(str2)) else false
+  }
+
+  def isMapsEqual(map1: Map[Char, Int], map2: Map[Char, Int]): Boolean = {
+    val rez = map1.foldLeft(0)((acc, curVal) => if (map2(curVal._1) != curVal._2) acc + 1 else acc)
+    rez == 0
+  }
+
+  def statisticMap(str: String): Map[Char, Int] = {
+    str.foldLeft(Map.empty[Char, Int])((map, key) => map + (key -> (map.getOrElse(key, 0) + 1)))
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(isStringsIdentical("tpo","pl"))
+  }
+}
