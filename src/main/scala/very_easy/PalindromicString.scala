3dc6b384fa0df237a1b53e777cbbc21af0cffcc1
sherstoi <sherstoi83@gmail.com>
2018-07-04 02:42:52 +0200

    PalindromicString

---
+++ src/main/scala/very_easy/PalindromicString.scala
@@ -0,0 +1,16 @@
+package very_easy
+
+import scala.collection.immutable.List
+
+object PalindromicString {
+  def isStringPalindrom(str: String): Boolean = {
+    val immutableList = str.foldLeft(List.empty[Char])((list, curChr) => curChr::list)
+    val reverseStr = immutableList.foldLeft("")((accumulator, curVal) => accumulator + curVal)
+
+    str == reverseStr
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(isStringPalindrom("level").toString)
+  }
+}
